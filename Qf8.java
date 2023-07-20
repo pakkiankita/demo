b5093caa6f6fea35146386fd86c4e5df57cc2cd2
pakkiankita <ankitapakki@gmail.com>
2023-07-20 21:08:00 +0530

    first commit

---
+++ Qf8.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Qf8
+{
+    public static double calPr(double x,double n)
+    {
+        double power=;
+        return power;
+    }
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    double x=sc.nextDouble();
+
+    double n=sc.nextDouble();
+
+
+    double power=calPr(x,n);
+    System.out.println(power);
+}
+}
